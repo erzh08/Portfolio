66d9ad0feb3ff856ba969ef2ef130c200a500616
Erzh 08 <erzh08@protonmail.com>
2022-05-15 20:22:21 -0300

    Website rewrite

--- script/anime.js
+++ script/anime.js
@@ -1,45 +1,48 @@
 window.onload = () => {
-	let anchors = document.getElementsByClassName('box');
+	let anchors = document.getElementsByClassName('nav-btn');
+	let boxes = document.getElementsByClassName('box');
 	let shadow = document.getElementById('shadow');
-	shadow.addEventListener('click', () => {
-		let boxes = document.getElementsByClassName('full');
-		gsap.to('.trans', {x: -3300, duration: 2});
-		setTimeout(() => {
-			boxes[0].classList.add('box');
-			boxes[0].classList.remove('full');
-			shadow.style.zIndex = 0;
-			shadow.style.width = "0px";
-			shadow.style.height = "0px";
-		}, 400);
-	});
 
 	/* Click on box */
 	for (const anchor of anchors) {
-		anchor.onclick = async() => {
+		anchor.onclick = async () => {
 			gsap.to('.trans', {x: 3300, duration: 1});
 			setTimeout(() => {
 				shadow.style.height = "100%";
 				shadow.style.width = "100%";
 				shadow.style.position = "absolute";
-				shadow.style.zIndex = 1;
-				anchor.classList.remove('box');
-				anchor.classList.add('full');
-			}, 430);
+				shadow.style.zIndex = 2;
 
-			switch (anchor.id) {
-				case "experience":
-					anchor.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url(media/aharen_reina_kr.png)";
+				switch (anchor.id) {
+				case "contact-nav":
+					document.getElementById("contact").style.display = "flex";
 					break;
-				case "experience":
-					anchor.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(media/aharen_reina_kr.png)";
+				case "projects-nav":
+					document.getElementById("projects").style.display = "flex";
 					break;
-				case "experience":
-					anchor.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(media/aharen_reina_kr.png)";
+				case "skills-nav":
+					document.getElementById("skills").style.display = "flex";
 					break;
-				case "experience":
-					anchor.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(media/aharen_reina_kr.png)";
+				case "about-nav":
+					document.getElementById("about").style.display = "flex";
 					break;
-			}
+				}
+			}, 430);
 		};
 	}
+
+	shadow.addEventListener('click', () => {
+		gsap.to('.trans', {x: -3300, duration: 2});
+		setTimeout(() => {
+			for (const box of boxes) {
+				if (box.style.display == "flex"){
+					box.style.display = "none";
+					break;
+				}
+			}
+			shadow.style.zIndex = 0;
+			shadow.style.width = "0px";
+			shadow.style.height = "0px";
+		}, 400);
+	});
 };
